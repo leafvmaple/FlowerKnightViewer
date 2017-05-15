f2898e52b8fb2c6eeab211e0f2242eeb455b6323
leafvmaple <leafvmaple@163.com>
2017-05-15 08:35:12 +0800

    Add Metro

---
+++ MetroRadiance/VS2012LikeWindow2/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MetroRadiance;
+
+namespace VS2012LikeWindow2
+{
+	public partial class App
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			ThemeService.Current.Initialize(this, Theme.Dark, Accent.Blue);
+		}
+	}
+}
